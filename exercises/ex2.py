10da8552dea8adb8d4b804298dbe3e67cbd5ab08
Alexander Kell <Alex@Alexanders-Mac-mini.local>
2018-08-24 06:46:04 -0400

    Completed a few exercises this morning

---
+++ exercises/ex2.py
@@ -0,0 +1,10 @@
+# comments and pound characters
+# a comment exists such that ou may read your program later.
+# anything after the # (octopthorpe or pound sign)  is ignored by python (and most things)
+
+print("I could have code like this.") # and the comment afteward is ignored
+
+# you may also use a comment to "disable" aor comment out code like ...
+# print ("This won't run")
+
+print ("but this will run")
---
+++ exercises/ex3.py
@@ -0,0 +1,35 @@
+# numbers and math, joy!
+# + is addition (plus)
+# - is subtraction (minus)
+# / is division (slash)
+# * is multiplication (asterisk)
+# & is remainder after division (modulous)
+# < is less than and > is greatter than
+# <= is less than or equal to and >= is greater than or equal to
+
+# remember the order of operations "PEMDAS" or Please Excuse My Dear Aunt Sally or Parenthesis Exponents Multiplication Division Addition and Subtraction
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs (before they hatch):")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False!")
+
+print("How about some more?")
+
+# true
+print("Is it greater?", 5 > -2)
+# true
+print("Is it greater or equal?", 5 >= -2)
+# false
+print("Is it less or equal to?", 5 <= -2)
