646fccd186a43f0d89efd5b546873adbcde669fd
A.T. Kell <alex@alexkell.com>
2018-09-09 17:04:07 -0400

    Working on a refactor and study drills

--- exercises/ex17.py
+++ exercises/ex17.py
@@ -6,22 +6,23 @@ from os.path import exists # import the exists method from the os.path module
 
 script, from_file, to_file = argv # unpack the contents of argv into these variables, si vous plez
 
-print(f"Copying from {from_file} to {to_file}")
+# print(f"Copying from {from_file} to {to_file}") # study drill 1: this script is annoying, try removing superflous lines where possible
 
 # we could do these two on one line, but how?
 in_file = open(from_file)
 indata = in_file.read()
+# indata = open(from_file, 'r')
 
-print(f"The input file is {len(indata)} bytes long")
+# print(f"The input file is {len(indata)} bytes long") # study drill 1: this script is annoying, try removing superflous lines where possible
 
-print(f"Does the output file really exist? {exists(to_file)}") # the exists module will return True or False here
-print("Hit RETURN to continue or ^C to abort.")
-input()
+# print(f"Does the output file really exist? {exists(to_file)}") # the exists module will return True or False here # study drill 1: this script is annoying, try removing superflous lines where possible
+# print("Hit RETURN to continue or ^C to abort.") # study drill 1: this script is annoying, try removing superflous lines where possible
+# input() # study drill 1: this script is annoying, try removing superflous lines where possible
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print("Alright, all done.")
+# print("Alright, all done.")  # study drill 1: this script is annoying, try removing superflous lines where possible
 
 out_file.close()
 in_file.close()
