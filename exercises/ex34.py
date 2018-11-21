872a1d5d2d43afc57fc61444f112f510614cc35c
A.T. Kell <alex@alexkell.com>
2018-11-20 19:50:13 -0500

    Getting started learning about accessing elements of a list

---
+++ exercises/ex34.py
@@ -0,0 +1,9 @@
+# accessing elenents of a list
+# kind of reminds me of php, accessing elements within an array by providing and index value
+# remember index == 0 is the start of any list
+
+animals = ["bear", "tiger", "lions", "flying howler monkey things"]
+bear = animals[0]
+
+print(animals[2])
+print(bear)
