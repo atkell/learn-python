bb579c71ee054007c7635db9bb65884683df276f
A.T. Kell <alex@alexkell.com>
2019-03-31 15:49:26 -0400

    I wasnt able to complete the game, but I did capture some great notes
    w

---
+++ exercises/ex45.py
@@ -0,0 +1,58 @@
+# You Make a Game
+
+# A note from Alex on 2019-03-31: Do to a variety of reasons I was
+# unable to complete this exercise. I'm moving ahead in the interest
+# of completeing this book.
+
+# I also took a moment to update some settings in Sublime.
+
+
+## Function Style
+# Programmers call functions that are part of classes, "methods". It's
+# annoying but you'll get used to it.
+
+# Instead of naming a function (method) within a class after what the
+# function does, consider naming it as if it is a command we are
+# giving to the class.
+
+# Keep functions small and simple. Don't forget this!
+
+
+## Class Style
+# A class should use "camel case" and not "underscore format".
+# ThisIsCamelCase
+
+# Do try and keep __init__ functions short and sweet.
+
+# The functions within the class should user "underscore format" like
+# my_awesome_dog and not MyAwesomeDog.
+
+
+# Do be consistent in how you organize your function arguments.
+
+# Variables should be self-contained and not from the modile or globals
+# where or whenever possible.
+
+# A foolish consistent is the hobggblin of little minds.
+# Think for your self.
+
+# Always, always, always have class Name(object) format or else you'll
+# be in "trouble".
+
+
+## Code Style
+# Readibility counts. Give yourself and your code vertical space.
+
+# If you cannot read it aloud, it's probably hard to read.
+
+# For now, emulate what others are doing in Python until you develop
+# your own style (see pep8).
+
+
+## Good Comments
+# Write comments. Ignore those who say otherwise.
+
+# When you write comments, describe WHY you are doing what you are
+# doing. Remember, the code itself already says HOW.
+
+# Don't forget to review your comments if you change your code.
